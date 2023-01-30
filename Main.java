b7019dfb54d1a7af0eed68df2f2d567c3915bb06
Neto Homework <clychnik@gmail.com>
2023-01-30 17:37:18 +0300

    all changes to restore defauls code

--- Main.java
+++ Main.java
@@ -11,8 +11,7 @@ public class Main {
         System.out.println("Сварили суп. На одного человека вышло:");
         System.out.println((water / eaters) + " миллилитров воды");
         System.out.println((potatoes / eaters) + " картофелин(а)");
-        System.out.println(((float) chicken / eaters) + " куриных(ое) бёдер(ро)");
+        System.out.println( chicken / eaters) + " куриных(ое) бёдер(ро)");
         System.out.println((spices / eaters) + " ложек(ка) специй");
-sout
     }
 }
